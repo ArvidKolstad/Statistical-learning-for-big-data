630b7eec06e7b161b9127b783189c45ff6d37d5c
Arvid Kolstad <kolstadarvid1@gmail.com>
2026-04-14 17:23:51 +0200

    borres ändringar

---
+++ project1/code/random_forest.py
@@ -0,0 +1,17 @@
+import numpy as np
+import sklearn
+from sklearn.ensemble import RandomForestClassifier
+
+
+def random_forest_classifier():
+
+    classifier = RandomForestClassifier()
+
+
+def main():
+    print("Hi")
+    random_forest_classifier()
+
+
+if __name__ == "__main__":
+    main()
