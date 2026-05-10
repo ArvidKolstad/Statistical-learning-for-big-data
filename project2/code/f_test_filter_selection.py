acc30344c3dcb3f16d47dc5d8a60d2e341c440ab
Arvid Kolstad <kolstadarvid1@gmail.com>
2026-05-10 17:36:07 +0200

    added some small changes in f_test_fulter

--- project2/code/f_test_filter_selection.py
+++ project2/code/f_test_filter_selection.py
@@ -2,25 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.feature_selection import SelectKBest, f_classif
 
-# ANOVA F-test filtering to k top pixels 
-def f_score_filter(images, labels, k, return_scores = False):
+
+def f_score_filter(images, labels, k, return_scores=False):
     filter = SelectKBest(f_classif, k=k)
     filter.fit(images, labels)
     filterd_images = filter.transform(images)
 
-    if return_scores: # For visualiztion
+    if return_scores:
         selected_pixel_idxs = filter.get_support(indices=True)
-        scores = filter.scores_ 
+        scores = filter.scores_
 
         return filterd_images, scores, selected_pixel_idxs
     else:
         return filterd_images
 
+
 def main():
     training_matrix = np.load("./data/train_matrix.npy")
     training_labels = np.load("./data/train_labels.npy")
 
-    filterd_images, scores, selected_pixel_idxs = f_score_filter(training_matrix, training_labels, 163, True)
+    filterd_images, scores, selected_pixel_idxs = f_score_filter(
+        training_matrix, training_labels, 163, True
+    )
 
     print(training_matrix.shape)
     print(filterd_images.shape)
@@ -30,12 +33,15 @@ def main():
     plt.figure()
     plt.imshow(pixels.reshape(64, 64), cmap="hot")
     plt.title("Choosen pixels")
+    plt.show()
+    plt.savefig("../figures/dim_filter/choosen_pixels.png")
 
     plt.figure()
     plt.imshow(scores.reshape(64, 64), cmap="hot")
     plt.colorbar()
     plt.title("F-score per pixel")
     plt.show()
+    plt.savefig("../figures/dim_filter/F_score_pre_pixel")
 
 
 if __name__ == "__main__":
