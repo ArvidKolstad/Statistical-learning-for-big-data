8b88101bcbee067d14721224f241650ff160bd5e
FilippaJS <sporref@chalmers.se>
2026-05-26 18:29:08 +0200

    kasln

--- project3/run_pipeline_xgb.py
+++ project3/run_pipeline_xgb.py
@@ -36,9 +36,9 @@ def main():
         model_settings,
         train_config,
     )
-    model_adapter = XGBModelAdapter(model_config, "./models/XGB_1000")
+    model_adapter = XGBModelAdapter(model_config, "./models/XGB_500")
 
-    df = pd.read_csv("./data/data_1000.csv")
+    df = pd.read_csv("./data/data_500.csv")
 
     out_data = df["label"].to_numpy()
     in_data = df.drop(columns="label").to_numpy()
