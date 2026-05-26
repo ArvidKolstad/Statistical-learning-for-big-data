c561ca9b5017843ad73382dcdd7993784cbbf86f
Arvid Kolstad <kolstadarvid1@gmail.com>
2026-05-26 16:05:30 +0200

    finished the plot files

--- project3/plot_images.py
+++ project3/plot_images.py
@@ -11,7 +11,7 @@ def load_data(paths):
     return array_dict
 
 
-def plot_performance(data):
+def plot_performance(data, samples):
     fig, ax = plt.subplots(1, 3, figsize=(16, 5))
     names = ["logreg", "KNN", "XGB", "RDA"]
     accuracy = np.vstack(
@@ -37,10 +37,10 @@ def plot_performance(data):
     ax[2].set_titel("AOC-curve")
 
     fig.tight_layout()
-    fig.savefig("figures_part1/preformace")
+    fig.savefig(f"figures_part1/preformace_{samples}")
 
 
-def plot_p_matrix(model_paths):
+def plot_p_matrix(model_paths, samples):
 
     accuracy = get_p_scores(model_paths, "accuracy", 10)
     f1 = get_p_scores(model_paths, "f1-score", 10)
@@ -48,20 +48,36 @@ def plot_p_matrix(model_paths):
 
     fig, ax = plt.subplots(1, 3, figsize=(16, 5))
 
-    ax[0].matshow(accuracy)
-    ax[1].boxplot(f1, labels=names)
-    ax[2].boxplot(aoc, labels=names)
+    names = ["logreg", "KNN", "XGB", "RDA"]
 
-    ax[0].set_ylabel("Accuracy")
-    ax[1].set_ylabel("F1-Score")
-    ax[2].set_ylabel("AOC")
+    ax[0].matshow(accuracy)
+    ax[0].set_xticks(range(len(names)))
+    ax[0].set_xticklabels(names)
+
+    ax[0].set_yticks(range(len(names)))
+    ax[0].set_yticklabels(names)
+
+    ax[1].matshow(f1)
+    ax[1].set_xticks(range(len(names)))
+    ax[1].set_xticklabels(names)
+
+    ax[1].set_yticks(range(len(names)))
+    ax[1].set_yticklabels(names)
+
+    ax[2].mathshow(aoc)
+    ax[2].set_xticks(range(len(names)))
+    ax[2].set_xticklabels(names)
+
+    ax[2].set_yticks(range(len(names)))
+    ax[2].set_yticklabels(names)
 
     ax[0].set_titel("Accuracy")
     ax[1].set_titel("F1-Score")
     ax[2].set_titel("AOC-curve")
 
+    fig.suptitle("P(model(i) > model(j))")
     fig.tight_layout()
-    fig.savefig("figures_part1/preformace")
+    fig.savefig(f"figures_part1/p_scores_{samples}.pdf")
 
 
 def main():
