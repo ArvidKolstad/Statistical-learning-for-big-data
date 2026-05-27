3f3dba3cfb2a64bbbf56e2cbb5c087627ce22918
Arvid Kolstad <kolstadarvid1@gmail.com>
2026-05-27 16:35:30 +0200

    plotted all the images

--- project3/performance_score.py
+++ project3/performance_score.py
@@ -1,23 +1,24 @@
 from baycomp import two_on_single
+from itertools import combinations
 import numpy as np
 
 
-def get_p_scores(models_paths, performance_score, runs, rope=0.01):
-
+def get_p_scores(model_paths, performance_score, runs, rope=0.01):
     index = get_performance_idx(performance_score)
-    matrix = np.zeros((4, 4))
+    p_lefts = []
+    p_ropes = []
+    p_rights = []
+    comparisons = combinations(model_paths, 2)
 
-    for idx1, path1 in enumerate(models_paths):
+    for path1, path2 in comparisons:
         model_1 = np.load("models/" + path1)[:, index]
-        print(path1)
-        print(model_1)
-        for idx2, path2 in enumerate(models_paths):
-            model_2 = np.load("models/" + path2)[:, index]
-            p_left, p_rope, p_right = two_on_single(
-                model_1, model_2, rope=rope, runs=runs
-            )
-            matrix[idx1, idx2] = p_rope
-    return matrix
+        model_2 = np.load("models/" + path2)[:, index]
+        p_left, p_rope, p_right = two_on_single(model_1, model_2, rope=rope, runs=runs)
+        p_lefts.append(p_left)
+        p_ropes.append(p_rope)
+        p_rights.append(p_right)
+
+    return p_lefts, p_ropes, p_rights
 
 
 def get_performance_idx(performance_score):
@@ -33,18 +34,23 @@ def get_performance_idx(performance_score):
 
 
 def get_rope_size(model_paths, performance_score, runs):
-    ropes = np.arange(0.0001, 0.1, 0.001)
+    ropes = np.arange(0.0001, 0.2, 0.001)
     index = get_performance_idx(performance_score)
+    comparisons = combinations(model_paths, 2)
+
     for rope in ropes:
         saved_p_ropes = []
-        for path1 in model_paths:
+        comparisons = combinations(model_paths, 2)
+        for path1, path2 in comparisons:
+
             model_1 = np.load("models/" + path1)[:, index]
-            for path2 in model_paths:
-                model_2 = np.load("models/" + path2)[:, index]
-                _, p_rope, _ = two_on_single(model_1, model_2, rope=rope, runs=runs)
-                saved_p_ropes.append(p_rope)
+            model_2 = np.load("models/" + path2)[:, index]
+
+            _, p_rope, _ = two_on_single(model_1, model_2, rope=rope, runs=runs)
+            saved_p_ropes.append(p_rope)
+
         mean_p_ropes = np.mean(saved_p_ropes)
-        if mean_p_ropes > 0.99:
+        if mean_p_ropes > 0.95:
             best_rope = rope
             print(f"Good rope was found: {best_rope}")
             return best_rope
@@ -57,16 +63,15 @@ def get_rope_size(model_paths, performance_score, runs):
 def main():
     samples = 1000
     runs = 10
-    rope = 0.00000001
-    models_paths = [
+    model_paths = [
         f"KNN_{samples}_random.npy",
         f"LogReg_{samples}_random.npy",
         f"XGB_{samples}_random.npy",
         f"RDA_{samples}_random.npy",
     ]
-    performance_score = "accuracy"
-    matrix = get_p_scores(models_paths, performance_score, runs, rope=rope)
-    print(matrix)
+    performance_score = "AOC"
+    # matrix = get_p_scores(models_paths, performance_score, runs, rope=rope)
+    get_rope_size(model_paths, performance_score, runs)
 
 
 if __name__ == "__main__":
--- project3/plot_images.py
+++ project3/plot_images.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def load_data(paths):
-    names = ["logreg", "KNN", "XGB", "RDA"]
+    names = ["KNN", "LogReg", "XGB", "RDA"]
     array_dict = {}
     for name, path in zip(names, paths):
         array_dict[name] = np.load("models/" + path)
@@ -15,16 +15,16 @@ def plot_performance(paths, samples):
 
     data = load_data(paths)
     fig, ax = plt.subplots(1, 3, figsize=(16, 5))
-    names = ["logreg", "KNN", "XGB", "RDA"]
+    names = ["KNN", "LogReg", "XGB", "RDA"]
     accuracy = np.vstack(
-        [data["logreg"][:, 0], data["KNN"][:, 0], data["XGB"][:, 0], data["RDA"][:, 0]]
+        [data["KNN"][:, 0], data["LogReg"][:, 0], data["XGB"][:, 0], data["RDA"][:, 0]]
     )
     f1 = np.vstack(
-        [data["logreg"][:, 1], data["KNN"][:, 1], data["XGB"][:, 1], data["RDA"][:, 1]]
+        [data["KNN"][:, 1], data["LogReg"][:, 1], data["XGB"][:, 1], data["RDA"][:, 1]]
     )
 
     aoc = np.vstack(
-        [data["logreg"][:, 2], data["KNN"][:, 2], data["XGB"][:, 2], data["RDA"][:, 2]]
+        [data["KNN"][:, 2], data["LogReg"][:, 2], data["XGB"][:, 2], data["RDA"][:, 2]]
     )
     ax[0].boxplot(accuracy.T, tick_labels=names)
     ax[1].boxplot(f1.T, tick_labels=names)
@@ -34,6 +34,10 @@ def plot_performance(paths, samples):
     ax[1].set_ylabel("F1-Score")
     ax[2].set_ylabel("AOC")
 
+    ax[0].set_ylim(0.4, 1)
+    ax[1].set_ylim(0.4, 1)
+    ax[2].set_ylim(0.4, 1)
+
     ax[0].set_title("Accuracy")
     ax[1].set_title("F1-Score")
     ax[2].set_title("AOC-curve")
@@ -42,48 +46,91 @@ def plot_performance(paths, samples):
     fig.savefig(f"figures_part1/preformace_{samples}")
 
 
-def plot_p_matrix(model_paths, samples):
+def plot_bayes_analysis(model_paths, samples):
 
-    accuracy = get_p_scores(model_paths, "accuracy", 10)
-    f1 = get_p_scores(model_paths, "f1-score", 10)
-    aoc = get_p_scores(model_paths, "AOC", 10)
+    acc_left, acc_rope, acc_right = get_p_scores(
+        model_paths, "accuracy", 10, rope=0.052
+    )
+    f1_left, f1_rope, f1_right = get_p_scores(model_paths, "f1-score", 10, rope=0.0821)
+    aoc_left, aoc_rope, aoc_right = get_p_scores(model_paths, "AOC", 10, rope=0.052)
 
-    fig, ax = plt.subplots(1, 3, figsize=(16, 5))
+    acc_stacks = {
+        "P(Left model > Right model)": acc_left,
+        "P(Left model = Right model)": acc_rope,
+        "P(Right model < Right model)": acc_right,
+    }
+    f1_stacks = {
+        "P(Left model > Right model)": f1_left,
+        "P(Left model = Right model)": f1_rope,
+        "P(Right model < Right model)": f1_right,
+    }
+    aoc_stacks = {
+        "P(Left model > Right model)": aoc_left,
+        "P(Left model = Right model)": aoc_rope,
+        "P(Right model < Right model)": aoc_right,
+    }
 
-    names = ["logreg", "KNN", "XGB", "RDA"]
+    fig, ax = plt.subplots(1, 3, figsize=(23, 5))
 
-    ax[0].matshow(accuracy)
-    ax[0].set_xticks(range(len(names)))
-    ax[0].set_xticklabels(names)
+    names = [
+        "KNN vs LogReg",
+        "KNN vs XGB",
+        "KNN vs RDA",
+        "LogReg vs XGB",
+        "LogReg vs RDA",
+        "XGB vs RDA",
+    ]
+    x = np.arange(len(names))
+    width = 0.2
+    multiplier = 0
+    for attribute, measurement in acc_stacks.items():
+        offset = width * multiplier
+        rects = ax[0].bar(x + offset, measurement, width, label=attribute)
+        # ax[0].bar_label(rects, padding=1)
+        multiplier += 1
 
-    ax[0].set_yticks(range(len(names)))
-    ax[0].set_yticklabels(names)
+    multiplier = 0
+    for attribute, measurement in f1_stacks.items():
+        offset = width * multiplier
 
-    ax[1].matshow(f1)
-    ax[1].set_xticks(range(len(names)))
-    ax[1].set_xticklabels(names)
+        rects = ax[1].bar(x + offset, measurement, width, label=attribute)
+        # ax[1].bar_label(rects, padding=1)
+        multiplier += 1
 
-    ax[1].set_yticks(range(len(names)))
-    ax[1].set_yticklabels(names)
+    multiplier = 0
+    for attribute, measurement in aoc_stacks.items():
+        offset = width * multiplier
 
-    ax[2].mathshow(aoc)
-    ax[2].set_xticks(range(len(names)))
-    ax[2].set_xticklabels(names)
+        rects = ax[2].bar(x + offset, measurement, width, label=attribute)
+        # ax[2].bar_label(rects, padding=1)
+        multiplier += 1
 
-    ax[2].set_yticks(range(len(names)))
-    ax[2].set_yticklabels(names)
+    ax[0].set_xticks(x + width, names)
+    ax[1].set_xticks(x + width, names)
+    ax[2].set_xticks(x + width, names)
+
+    ax[0].set_ylim(0, 1.3)
+    ax[1].set_ylim(0, 1.3)
+    ax[2].set_ylim(0, 1.3)
+
+    ax[0].set_ylabel("Probability")
+    ax[1].set_ylabel("Probability")
+    ax[2].set_ylabel("Probability")
 
     ax[0].set_title("Accuracy")
     ax[1].set_title("F1-Score")
-    ax[2].set_title("AOC-curve")
+    ax[2].set_title("AOC")
 
-    fig.suptitle("P(model(i) > model(j))")
+    ax[0].legend()
+    ax[1].legend()
+    ax[2].legend()
+
     fig.tight_layout()
-    fig.savefig(f"figures_part2/p_scores_{samples}.pdf")
+    fig.savefig(f"./figures_part2/barchart_{samples}.png")
 
 
 def main():
-    samples = 250
+    samples = 500
     model_paths = [
         f"KNN_{samples}.npy",
         f"LogReg_{samples}.npy",
@@ -91,6 +138,7 @@ def main():
         f"RDA_{samples}.npy",
     ]
     plot_performance(model_paths, samples)
+    # plot_bayes_analysis(model_paths, samples)
 
 
 if __name__ == "__main__":
