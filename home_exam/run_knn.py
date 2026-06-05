453db23da37f3b2d817bbd4fd4876f03b4021de5
Arvid Kolstad <kolstadarvid1@gmail.com>
2026-06-05 13:11:12 +0200

    changed runfile knn

--- home_exam/run_knn.py
+++ home_exam/run_knn.py
@@ -40,12 +40,13 @@ def main():
     )
     sizes = [1, 3, 5, 15, 20, 40]
 
-    for size in sizes:
-        # samples = [200, 500, 1000, 3000, 5000, 7680]
-        sample = 7680
+    # for size in sizes:
+    sample = 7680
 
-        # splits = 10
-        # for sample in samples:
+    samples = [200, 500, 1000, 3000, 5000, 7680]
+
+    # splits = 10
+    for sample in samples:
         model_adapter = KNNModelAdapter(
             model_config,
             f"./models/1a/KNN_{sample}",
@@ -55,8 +56,8 @@ def main():
         test_data = np.load(f"./data/test_set.npy")
 
         train_data = [train_inputs, train_labels]
-        # run_pipeline(model_adapter, train_data)
-        run_defect_pipeline(model_adapter, train_data, size)
+        run_pipeline(model_adapter, train_data)
+        # run_defect_pipeline(model_adapter, train_data, size)
 
     # kCV_outer(model_adapter, train_data, multiple_runs=0)
     # with open(model_adapter.output_dir + "config.pickle", "rb") as f:
